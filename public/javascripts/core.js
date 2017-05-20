0cf6cbb5c93f59616e4dc555083a9fc99fc1c406
komogortev <komogortev@gmail.com>
2017-05-20 04:08:09 +0000

    Added drag & drop support for columns

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -18,11 +18,90 @@ Core = {
 
     Core.listenSettings();
 
-    Core.toggleHandler();
+    Core.handleDrag();
+
+    Core.handleToggle();
 
   },
 
 };
+
+Core.handleDrag = function coreHandleDrag(){
+  var dragged;
+
+  $(document).on("dragstart", function( event ) {
+      $('.js-drop-box').addClass('drag-on');
+      dragged = event.target;
+      $(dragged).addClass('in-transit');
+  });
+
+  $(document).bind("dragover", function( event ) {
+      event.preventDefault();
+  });
+
+  $(document).on("drop", function( event ) {
+      event.preventDefault();
+      $('.js-drop-box').removeClass('drag-on');
+      $(dragged).removeClass('in-transit');
+      var donor = $(dragged).closest('.js-drop-box');
+      var recipient = $(event.target).closest('.js-drop-box');
+      var replacement = $(event.target).closest('.js-drop-box').find('.js-drag-box');
+
+      donor.remove('.js-drag-box').append(replacement);
+      recipient.remove('.js-drag-box').append( dragged );
+  });
+}
+
+Core.handleToggle = function coreHandleToggle() {
+
+  $('.js-toggle').off('click').on('click', function toggleTarget() {
+
+    var $target = $($(this).data('target'));
+
+    if ($target.hasClass('is-active')) {
+      $target.removeClass('is-active');
+    } else {
+      $target.addClass('is-active');
+    }
+
+  });
+
+};
+
+Core.sortUsers = function coreSortUsers(order) {
+  //having given 3 user feeds is required amount at all times
+  //otherwise dynamic assigning in loop through the children required
+  var userFeeds = $('.user-feed').find('.js-feed-container');
+
+
+  switch (order) {
+    case 'order-asc':
+
+      userFeeds.each(function(index){
+        $(this).css('order', index + 1);
+      });
+
+      break;
+    case 'order-desc':
+
+      userFeeds.each(function(index){
+        $(this).css('order', userFeeds.length - index);
+        console.log(userFeeds.length - index)
+      });
+
+      break;
+    case 'order-mix':
+      for(var i, tmpArr=[i=1]; i<userFeeds.length;tmpArr[i++]=parseInt(Math.random() * i));
+      userFeeds.each(function(index){
+        $(this).css('order', tmpArr[index]);
+      });
+      break;
+    default:
+      return false;
+    break;
+  }
+
+}
 
 Core.resetSettings = function coreResetSettings() {
 
@@ -79,65 +158,5 @@ Core.listenSettings = function coreListenSettings() {
 Core.initSettings = function coreInitSettings() {
     $('body').addClass(Core.params.pageSettingsCollection);
 };
-
-
-
-Core.toggleHandler = function coreToggleHandler() {
-
-  $('.js-toggle').off('click').on('click', function toggleTarget() {
-
-    var $target = $($(this).data('target'));
-
-    if ($target.hasClass('is-active')) {
-      $target.removeClass('is-active');
-    } else {
-      $target.addClass('is-active');
-    }
-
-  });
-
-};
-
-Core.sortUsers = function coreSortUsers(order) {
-  //having given 3 user feeds is required amount at all times
-  //otherwise dynamic assigning in loop through the children required
-  var userFeeds = $('.user-feed').find('.js-feed-container');
-
-
-  switch (order) {
-    case 'order-asc':
-
-      userFeeds.each(function(index){
-        $(this).css('order', index + 1);
-      });
-
-      break;
-    case 'order-desc':
-
-      userFeeds.each(function(index){
-        $(this).css('order', userFeeds.length - index);
-        console.log(userFeeds.length - index)
-      });
-
-      break;
-    case 'order-mix':
-      for(var i, tmpArr=[i=1]; i<userFeeds.length;tmpArr[i++]=parseInt(Math.random() * i));
-      userFeeds.each(function(index){
-        $(this).css('order', tmpArr[index]);
-      });
-      break;
-    default:
-      return false;
-    break;
-  }
-
-
-
-
-  /*userFeeds.forEach(function callback(currentValue, index, array) {
-      $(currentValue).css('order', index);
-  });*/
-}
-
 
 Core.init();
